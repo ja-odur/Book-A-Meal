f762e41374c9d1f27d445fe864706a3c52ec6e47
Odur Joseph <odurjoseph8@gmail.com>
2018-04-26 18:55:13 +0300

    setup test cases for delete meal endpoint.

--- backend/test_meals.py
+++ backend/test_meals.py
@@ -47,6 +47,20 @@ class TestMeals(unittest.TestCase):
         self.assertEqual(get_response.status_code, 201)
         self.assertEqual(expected_response_message, response_results['message'])
 
+    def test_delete_meal(self):
+        input_data = dict(username='default', name='meal', price=5000)
+        # update_data = dict(price=6000)
+        expected_response_message = [1, 'meal', 6000]
+        self.tester.post('api/v1/meals/', content_type="application/json", data=json.dumps(input_data))
+        self.tester.post('api/v1/meals/', content_type="application/json", data=json.dumps(input_data))
+        get_response = self.tester.delete('api/v1/meals/2')
+
+        # response_results = json.loads(get_response.data.decode())
+        # print(response_results)
+
+        self.assertEqual(get_response.status_code, 201)
+        # self.assertEqual(expected_response_message, response_results['message'])
+
 
 if __name__ == '__main__':
     unittest.main()
