44ac33430bba2acd7be57a961d98454aafb1a414
Odur Joseph <odurjoseph8@gmail.com>
2018-04-26 15:52:38 +0300

    added a field on the signup page

--- backend/apis_book_a_meal.py
+++ backend/apis_book_a_meal.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, make_response
-from models import DbUsers, DbCaterers
+from .models import DbUsers, DbCaterers
 
 app = Flask(__name__)
 user_db = DbUsers()
@@ -15,6 +15,10 @@ def register_user():
                 message = '{} successfully signed up.'.format(data['username'])
                 message.encode('utf-8')
                 return make_response(jsonify(dict(message=message)), 201)
+            else:
+                message = '{} already exists.'.format(data['username'])
+                message.encode('utf-8')
+                return make_response(jsonify(dict(message=message)), 401)
 
         elif data['category'] == 'caterer':
             if caterer_db.add_user(data['email'], data['username'], data['password'], data['address'],
--- backend/test.py
+++ backend/test.py
@@ -2,14 +2,14 @@ import unittest
 import json
 
 from .apis_book_a_meal import app
-from models import DbUsers, DbCaterers
 
 
 class TestAPIs(unittest.TestCase):
     def setUp(self):
         self.tester = app.test_client(self)
-        # user_db = DbUsers()
-        # caterer_db = DbCaterers()
+
+    # def tearDown(self):
+    #     del
 
     def test_successful_user_registration(self):
         input_data = dict(category='user', email='default@gmail.com', username='default', password='12345',
@@ -34,7 +34,21 @@ class TestAPIs(unittest.TestCase):
         self.assertEqual(get_response.status_code, 201)
         self.assertEqual(expected_response_message, response_results['message'])
 
+    def test_duplicate_user_sign_up(self):
+        input_data = dict(category='user', email='default@gmail.com', username='default', password='12345',
+                          confirm_password='12345', address='address1')
 
+        # initialsignup
+        get_response1 = self.tester.post('/auth/signup', content_type="application/json", data=json.dumps(input_data))
+        expected_response_message = '{} already exists.'.format(input_data['username'])
+
+        #second signup
+        get_response3 = self.tester.post('/auth/signup', content_type="application/json", data=json.dumps(input_data))
+
+        response_results = json.loads(get_response3.data.decode())
+        print(get_response3.status_code)
+        self.assertEqual(get_response3.status_code, 401)
+        self.assertEqual(expected_response_message, response_results['message'])
 
 
 if __name__ == '__main__':
