97a0689a803911d015f26451021ef88f483e43df
Odur Joseph <odurjoseph8@gmail.com>
2018-04-27 00:16:49 +0300

    created test cases for post menu end point

---
+++ backend/test_menu.py
@@ -0,0 +1,36 @@
+import unittest
+import json
+
+from .api import app
+
+class TestMenu(unittest.TestCase):
+    def setUp(self):
+        self.tester = app.test_client(self)
+        # self.get_response = None
+
+    def test_create_menu(self):
+        menu = [[1, 'rice and posho', 5000], [1, 'rice and posho', 5000], [1, 'rice and posho', 5000]]
+        input_data = dict(menu=menu)
+        expected_response_message = 'Meal {} successfully added.'.format(input_data['name'])
+        get_response = self.tester.post('api/v1/menu/', content_type="application/json", data=json.dumps(input_data))
+
+        response_results = json.loads(get_response.data.decode())
+        # print(response_results)
+
+        self.assertEqual(get_response.status_code, 201)
+        self.assertEqual(expected_response_message, response_results['message'])
+
+    # def get_menu(self):
+    #     menu = [[1, 'rice and posho', 5000], [1, 'rice and posho', 5000], [1, 'rice and posho', 5000]]
+    #     input_data = dict(menu=menu)
+    #     expected_response_message = 'Meal {} successfully added.'.format(input_data['name'])
+    #     get_response = self.tester.post('api/v1/menu/', content_type="application/json", data=json.dumps(input_data))
+    #
+    #     response_results = json.loads(get_response.data.decode())
+    #     # print(response_results)
+    #
+    #     self.assertEqual(get_response.status_code, 201)
+    #     self.assertEqual(expected_response_message, response_results['message'])
+
+if __name__ == '__main__':
+    unittest.main()
