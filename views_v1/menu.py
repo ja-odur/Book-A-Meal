5689e2c1e003f8495f1f93f3d2400c3ead043796
Odur Joseph <odurjoseph8@gmail.com>
2018-04-29 00:13:50 +0300

    created more tests for menu end points

--- views_v1/menu.py
+++ views_v1/menu.py
@@ -1,7 +1,18 @@
-from flask import Blueprint
+from flask import jsonify, request, make_response, Blueprint
 
 from models_v1.models import DbMenu
 
 menu_db = DbMenu()
 
 menu = Blueprint('menu', __name__, url_prefix='/api/v1')
+
+
+@menu.route('/menu/', methods=['POST'])
+def create_menu():
+    data = request.get_json()
+    created_menu = menu_db.create_menu(caterer='default', daily_menu=data['menu'])
+
+    if created_menu:
+        return make_response(jsonify(message=data['menu']), 201)
+    return make_response(jsonify(message='Bad data format'), 403)
+
