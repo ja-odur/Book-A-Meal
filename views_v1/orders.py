cbac3543d1d9873ec801858475dcba0ac0111a6e
Odur Joseph <odurjoseph8@gmail.com>
2018-04-29 12:54:09 +0300

    successful create order endpoint

--- views_v1/orders.py
+++ views_v1/orders.py
@@ -2,6 +2,17 @@ from flask import jsonify, request, make_response, Blueprint
 
 from models_v1.models import DbOrders
 
-orders_db = DbOrders
+orders_db = DbOrders()
 
 orders = Blueprint('orders', __name__, url_prefix='/api/v1')
+
+@orders.route('/orders', methods=['POST'])
+def create_order():
+    data = request.get_json()
+    if data['caterer'] and data['meal']:
+        new_order = orders_db.add_order('default', data['caterer'], data['meal'])
+
+        if new_order:
+            message = 'Order {} successfully placed.'.format(data)
+            return make_response(jsonify(message=message), 201)
+    return make_response(jsonify(message='Invalid request format'), 403)
