9dd269c11548cc88fe98f78e1d1740397bc2f5c9
Odur Joseph <odurjoseph8@gmail.com>
2018-04-30 17:07:25 +0300

    built tests for order end api

--- tests_v1/test_orders.py
+++ tests_v1/test_orders.py
@@ -1,6 +1,8 @@
 import unittest
 import json
 
+import datetime
+
 from run import app
 
 class TestMenu(unittest.TestCase):
@@ -8,7 +10,7 @@ class TestMenu(unittest.TestCase):
         self.tester = app.test_client(self)
         # self.get_response = None
 
-    def test_create_order(self):
+    def test_create_order_success(self):
         order = dict(meal=[1, 'rice and posho', 5000], caterer='default')
 
         expected_response_message = 'Order {} successfully placed.'.format(order)
@@ -20,22 +22,36 @@ class TestMenu(unittest.TestCase):
         self.assertEqual(get_response.status_code, 201)
         self.assertEqual(expected_response_message, response_results['message'])
 
-    def test_modify_order(self):
-        order = dict(meal=[1, 'rice and posho', 5000], caterer='default')
-        modified_order = dict(default=dict(meal=[1, 'rice and posho_modified', 5000], caterer='default', orderId=1))
-        expected_response_message = 'Menu {} successfully added.'.format(order)
-        self.tester.post('api/v1/orders', content_type="application/json", data=json.dumps(order))
-        get_response = self.tester.put('api/v1/orders/orderId', content_type="application/json", data=json.dumps(modified_order))
+    def test_create_order_failure(self):
+        order = dict(meal3=[1, 'rice and posho', 5000], caterer='default')
+
+        expected_response_message = 'Invalid request format'
+        get_response = self.tester.post('api/v1/orders', content_type="application/json", data=json.dumps(order))
 
         response_results = json.loads(get_response.data.decode())
         # print(response_results)
 
-        self.assertEqual(get_response.status_code, 201)
+        self.assertEqual(get_response.status_code, 403)
         self.assertEqual(expected_response_message, response_results['message'])
 
-    def test_get_all_orders(self):
-        expected_response_message = 'Menu {} successfully added.'.format('set value')
-        get_response = self.tester.put('api/v1/orders')
+    def test_modify_order(self):
+        order = dict(meal=[1, 'rice and posho', 5000], caterer='default3')
+        modified_order = dict(meal=[1, 'rice and posho_modified', 5000], caterer='default3')
+        expected_response_message = 'Menu {} successfully added.'.format(order)
+        self.tester.post('api/v1/orders', content_type="application/json", data=json.dumps(order))
+        get_response = self.tester.put('api/v1/orders/1', content_type="application/json", data=json.dumps(modified_order))
+
+        # response_results = json.loads(get_response.data.decode())
+        # print(response_results)
+
+        self.assertEqual(get_response.status_code, 201)
+        # self.assertEqual(expected_response_message, response_results['message'])
+
+
+
+    def test_get_all_orders_failure(self):
+        expected_response_message = 'Oops, orders not found.'
+        get_response = self.tester.get('api/v1/orders')
 
         response_results = json.loads(get_response.data.decode())
         # print(response_results)
@@ -43,6 +59,17 @@ class TestMenu(unittest.TestCase):
         self.assertEqual(get_response.status_code, 200)
         self.assertEqual(expected_response_message, response_results['message'])
 
+    def test_get_all_orders_successful(self):
+        order = dict(meal=[1, 'rice and posho', 5000], caterer='default4')
+        expected_response_message = 'The request was successfull'
+        self.tester.post('api/v1/orders', content_type="application/json", data=json.dumps(order))
+        get_response = self.tester.get('api/v1/orders')
+
+        response_results = json.loads(get_response.data.decode())
+
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(expected_response_message, response_results['message'])
+
 
 if __name__ == '__main__':
-    unittest.run()
+    unittest.main()
