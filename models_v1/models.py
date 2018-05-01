651d3f516908bacb901eccd697d88cec2f4961c2
Odur Joseph <odurjoseph8@gmail.com>
2018-05-01 08:50:55 +0300

    modified DbOrder -fixed bug

--- models_v1/models.py
+++ models_v1/models.py
@@ -160,13 +160,15 @@ class DbOrders:
     def __init__(self):
         self.orders_customers = dict()
         self.orders_caterers = dict()
-        self.order_expiry_time = datetime.timedelta(hours=1).total_seconds()
+        self.order_expiry_time = datetime.timedelta(minutes=1).total_seconds()
         self.id_count = 1
 
     def add_order(self, customer, caterer, meal):
         order_time = datetime.datetime.now()
-        order_customer_format = dict(order_id=self.id_count, meal=meal, caterer=caterer, cleared=False, order_time=order_time)
-        order_caterer_format = dict(order_id=self.id_count, meal=meal, customer=customer, cleared=False, order_time=order_time)
+        order_customer_format = dict(order_id=self.id_count, meal=meal, caterer=caterer, cleared=False,
+                                     order_time=order_time)
+        order_caterer_format = dict(order_id=self.id_count, meal=meal, customer=customer, cleared=False
+                                    , order_time=order_time)
         self.id_count += 1
         set_customer_order = False
         set_caterer_order = False
@@ -196,35 +198,48 @@ class DbOrders:
     def modify_order(self, customer, caterer, order_id, meal):
         time_now = datetime.datetime.now()
         try:
+            # print('in try block')
             orders = self.orders_customers[customer]
+            orders_caterers = self.orders_caterers[caterer]
         except KeyError:
             pass
         else:
+            # print('in try-except-else block')
+            match_found = False
+            matched_caterer_found = False
             counter_customer = 0
             counter_caterer = 0
 
             while counter_customer < len(orders):
+                # print('in loop_customer')
                 if orders[counter_customer]['order_id'] == order_id:
+                    match_found = True
+                    # print('found_customer')
                     break
                 counter_customer += 1
 
-            # while counter_caterer < len(orders_caterer):
-            #     if orders_caterer[counter_caterer]['order_id'] == order_id and \
-            #                       orders_caterer[counter_caterer]['meal'] == meal:
-            #         break
-            #     counter_caterer += 1
+            while counter_caterer < len(orders_caterers):
+                # print('inloop_caterer')
+                if orders_caterers[counter_caterer]['order_id'] == order_id and \
+                                  orders_caterers[counter_caterer]['customer'] == customer:
+                    matched_caterer_found = True
+                    # print('found_caterer')
+                    break
+                counter_caterer += 1
 
-            order_time = self.orders_customers[customer][counter_customer]['order_time']
-            elapse_time = time_now - order_time
-            if elapse_time.total_seconds() <= self.order_expiry_time:
-                self.orders_customers[customer][counter_customer]['meal'] = meal
-                self.orders_caterers[caterer][counter_caterer]['meal'] = meal
+            if match_found and matched_caterer_found:
+                order_time = self.orders_customers[customer][counter_customer]['order_time']
+                elapse_time = time_now - order_time
+                if elapse_time.total_seconds() <= self.order_expiry_time:
+                    self.orders_customers[customer][counter_customer]['meal'] = meal
+                    self.orders_caterers[caterer][counter_caterer]['meal'] = meal
 
-                return True
-            else:
-                return 'not found'
-            # return False
+                    return True
+                else:
+                    return False
+                    # return 'elapsed time'
 
+        # return 'end hit'
         return False
 
 
