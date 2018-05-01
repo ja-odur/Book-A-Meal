e7252c63e406c24966e35594eddfa2a9528bbf41
Odur Joseph <odurjoseph8@gmail.com>
2018-05-01 19:16:51 +0300

    setup swagger

--- run.py
+++ run.py
@@ -1,21 +1,21 @@
 from flask import Flask
+from flasgger import Swagger
+from app_v1.views.users import users
+from app_v1.views.meals import meals
+from app_v1.views.menu import menu
+from app_v1.views.orders import orders
 
 app = Flask(__name__, instance_relative_config=True)
 
-
-from app_v1.views.users import users
+SWAGGER = Swagger(app)
 
 app.register_blueprint(users)
 
-from app_v1.views.meals import meals
 app.register_blueprint(meals)
 
-from app_v1.views.menu import menu
 app.register_blueprint(menu)
 
-from app_v1.views.orders import orders
 app.register_blueprint(orders)
-
 
 
 if __name__ == '__main__':
